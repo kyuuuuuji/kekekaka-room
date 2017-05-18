041b66d21e818341f47893d3fd04e88cd6c5d67a
Fujikawa <ts-yusuke.a.fujikawa@rakuten.com>
2017-05-18 16:36:02 +0900

    success import chart.js from sample.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URL);
+// mongoose.connect(process.env.MONGO_URL);
+mongoose.connect('mongodb://kekekaka-room:NUUUl1kmuIitFdIzZ4ALBhMwG29lwo5lUhCAsWQyhxkwEjRhiVGz2MfJ6h2JdyUZ8FqxybmV6zQLV5OGF6QI6A==@kekekaka-room.documents.azure.com:10255/?ssl=true');
 
 var Room = require('../models/room');
 
