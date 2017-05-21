ae3836b3ecefb8a534aaecf6a711bab619972460
kyuuuuuji <fujikaway0904@hotmail.co.jp>
2017-05-22 01:02:16 +0900

    とんでもない定期

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-// mongoose.connect(process.env.MONGO_URL);
-mongoose.connect('mongodb://kekekaka-room:NUUUl1kmuIitFdIzZ4ALBhMwG29lwo5lUhCAsWQyhxkwEjRhiVGz2MfJ6h2JdyUZ8FqxybmV6zQLV5OGF6QI6A==@kekekaka-room.documents.azure.com:10255/?ssl=true');
+mongoose.connect(process.env.MONGO_URL);
 
 var Room = require('../models/room');
 
